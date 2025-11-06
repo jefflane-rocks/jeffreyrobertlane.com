f04fe8a44f6495daafd760c8289fa802cc95384c
Jeff Lane <jeff@jeffreyrobertlane.com>
2025-11-06 14:27:25 -0800

    adding fields to audioplayer, JSON of song list

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,108 @@
 import Image from "next/image";
 import { AudioPlayer } from "@/components/AudioPlayer";
 
+const AudioPlayerList = [
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/Move.mp3",
+    title: "Move",
+    artist: "Burningdown",
+    genre: "Alternative Rock",
+    description: "An epic rock ballad about moving on and starting again",
+    tags: "Break away, Start Over, Reinvent, Move, Find yourself",
+  },
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/01%20Carry%20On.mp3",
+    title: "Carry On",
+    artist: "Burningdown",
+    genre: "Alternative Rock",
+    description: "A song about starting over, finding yourself",
+    tags: "Break away, Start Over, Reinvent, Determination, Find yourself",
+  },
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/03%20Fall.mp3",
+    title: "Fall",
+    artist: "Burningdown",
+    genre: "Alternative Rock",
+    description:
+      "A head in hands lament of selfish mistakes; a confession and apology.",
+    tags: "Sorrow, Mistake, Apology, Lament, Unfaithfulness, Broken Relationship, Cheater",
+  },
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/04%20Miracles.mp3",
+    title: "Miracles",
+    artist: "Burningdown",
+    genre: "Alternative Rock",
+    description:
+      "A song of faith and optimism that your dreams can come true if you believe and chase after them.",
+    tags: "Faith, Optimism, Hope, Miracles, Dream",
+  },
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/05%20Fifteen.mp3",
+    title: "Fifteen",
+    artist: "Burningdown",
+    genre: "Alternative Rock",
+    description: "A song empathizing with teenage angst and insecurities.",
+    tags: "Fit in, Peer Pressure, Angst, Insecure, High School, Teenager, Teen, Teens",
+  },
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/06%20Remember.mp3",
+    title: "Remember",
+    artist: "Burningdown",
+    genre: "Alternative Rock",
+    description:
+      "A pledge of love and faithfulness even when separated by time and distance.",
+    tags: "Remember, Faithfulness, Love, Promise, Hope, Dedication, Missing You",
+  },
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/07%20Anything.mp3",
+    title: "Anything",
+    artist: "Burningdown",
+    genre: "Alternative Rock",
+    description:
+      "A song of devotion and willingness to sacrifice everything for love.",
+    tags: "Devotion, Sacrifice, Love, Dedication, For richer or poorer, In sickness and in health",
+  },
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/08%20Romeo.mp3",
+    title: "Romeo",
+    artist: "Burningdown",
+    genre: "Alternative Rock",
+    description: "A song about forbidden love.",
+    tags: "Forbidden, Love, Elope, Escape",
+  },
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/09%20Satellite.mp3",
+    title: "Satellite",
+    artist: "Burningdown",
+    genre: "Alternative Rock",
+    description:
+      "A song inspired by driving on Pacific Coast Highway on a summer evening.",
+    tags: "Summer, Convertible, Ocean, PCH, Southern California, Los Angeles, Redondo Beach, Malibu, Hermosa Beach, Manhattan Beach",
+  },
+  {
+    src: "https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/10%20May%20The%20Road%20Bring%20You%20Home.mp3",
+    title: "May the Road Bring You Home",
+    artist: "Burningdown",
+    genre: "Folk",
+    description: "A blessing, a prayer for your guidance.",
+    tags: "Hymn, Prayer, Blessing, Direction, Guidance, Inner Voice, Heart",
+  },
+];
+
 export default function Home() {
   return (
     <div>
-      <div>Test Deploy</div>
-      <AudioPlayer
-        src="https://vnshanftypzvajpbbwxr.supabase.co/storage/v1/object/public/mp3s/Move.mp3"
-        title="Move by Burningdown"
-        description="Epic Rock Ballad about moving on and starting again"
-        tags="Break away, Start Over, Reinvent, Move, Find yourself"
-      />
+      <div>JeffLane.Rocks</div>
+      {AudioPlayerList.map((item) => (
+        <AudioPlayer
+          key={item.title}
+          title={item.title}
+          genre={item.genre}
+          artist={item.artist}
+          description={item.description}
+          src={item.src}
+        />
+      ))}
     </div>
   );
 }
--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,21 +1,25 @@
 function AudioPlayer({
   src,
   title,
+  artist,
+  genre,
   description,
   tags,
   youTubeTitle,
   youTubeLink,
 }) {
   return (
-    <div>
-      <div>{title}</div>
-      <div>{description}</div>
-      <div>
-        <audio controls>
+    <div className="grid grid-flow-col-dense grid-cols-5">
+      <div className="col-span-1">
+        <audio id={`id${src}`}>
           <source src={src} type="audio/mpeg" />
           Your browser does not support the audio element. Bummer.
         </audio>
       </div>
+      <div className="col-span-1">{title}</div>
+      <div className="col-span-1">{artist}</div>
+      <div className="col-span-1">{genre}</div>
+      <div className="col-span-1">{description}</div>
     </div>
   );
 }
