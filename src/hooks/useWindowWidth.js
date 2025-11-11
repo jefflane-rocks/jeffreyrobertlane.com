afed1fe29d19f08391e5a9a742dd665c5d890247
Jeff Lane <jeff@jeffreyrobertlane.com>
2025-11-11 15:35:03 -0800

    fixed window prerender error in hook

--- src/hooks/useWindowWidth.js
+++ src/hooks/useWindowWidth.js
@@ -2,21 +2,15 @@
 import { useState, useEffect } from "react";
 
 function useWindowWidth() {
-  const [windowWidth, setWindowWidth] = useState(
-    window ? window.innerWidth : 0
-  );
+  const [windowWidth, setWindowWidth] = useState(0);
 
   useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window ? window.innerWidth : 0);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    if (typeof window !== "undefined") {
+      setWindowWidth(window.innerWidth);
+      const handleResize = () => setWindowWidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
   }, []); // Empty dependency array ensures the effect runs only once on mount
 
   return windowWidth;
